a564713aca1c96d961813b613717af0e20e14301
YENO0 <ong102367@gmail.com>
2025-12-16 00:36:27 +0800

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php 
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StudentController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function(){
+    return ['message'=> 'API is working'];
+});
+
+Route::apiResource('students', StudentController::class);
+
+?>
